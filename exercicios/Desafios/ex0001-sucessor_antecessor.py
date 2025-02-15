b9ab75aecb94a87e991c7a3e23892cbd1f3087a8
Josêmar Lino <josemars.lino@gmail.com>
2025-02-15 15:20:06 -0300

    Correção de bug, criação de arquivo

---
+++ exercicios/Desafios/ex0001-sucessor_antecessor.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número:'))
+s = n1 + 1
+a = n1 - 1
+print('O seu sucessor será : {} \n O seu antecessor será:{}'.format(s, a))
--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -9,5 +9,5 @@ d= n1 / n2
 di= n1 // n2
 e= n1 ** n2
 
-print('A Soma vale {}, \nProduto é {} e a Divisão é {:.3f}'.format(s, m, d),end= '')
+print('A Soma vale {}, \n Produto é {} e a Divisão é {:.3f}'.format(s, m, d),end= '')
 print('Divisão inteira {}, e potência {}'.format(di, e))
